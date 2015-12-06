33693fd53ff45ba846cf26847fe528b68d469446
fonylew <kamolphan.fony@gmail.com>
2015-12-06 22:58:11 +0700

    ratio coin calculation - fail in test 2,3,4

--- main.cpp
+++ main.cpp
@@ -79,13 +79,16 @@ int main(){
         // EXPERIMENTAL
         vector<int> type;
         int ntype = 0;
-        map<int,int> valuencoin;
+        float sum = 0.0;
+        map<float,int> valuencoin;
+        vector<float> value {10,5,2,1,0.5,0.25};
+        vector<float> tenratio {1.0834,1.1955,1.31,1.45,1.625};
 
         // Draw the circles detected
         for( size_t i = 0; i < circles.size(); i++ ){
             Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
             int radius = cvRound(circles[i][2]);
-            circle( src, center, 3, Scalar(0,0,255), -1, 8, 0 );// circle center
+            //circle( src, center, 3, Scalar(0,0,255), -1, 8, 0 );// circle center
             circle( src, center, radius, Scalar(0,255,0), 1, 8, 0 );// circle outline
             coins[i] = Coin(i,radius,circles[i][0],circles[i][1]);
             string a = to_string(i)+":"+to_string(radius);
@@ -111,8 +114,10 @@ int main(){
         cout<<endl<<"sort : ";
 
         //Initial value
-        int prev_rad = 0;
+        int prev_rad = 0, tentype=0 ,vindex=0;
+        float ten_rad=0;
         float diff_rad, weight_diff, min_diff;
+        bool hasTen = false;
         //There exists a coin
         if(circles.size()>0) {
             ntype = 1;
@@ -123,22 +128,58 @@ int main(){
         }
         for(int i=0;i<circles.size();i++){
             //count types
+            //new type
             if(prev_rad - coins[i].radius > diff_rad){
+                if(ten_rad/coins[i].radius < tenratio[1]) vindex = 1;       //5 baht
+                else if(ten_rad/coins[i].radius < tenratio[2]) vindex = 2;  //2 baht
+                else if(ten_rad/coins[i].radius < tenratio[3]) vindex = 3;  //1 baht
+                else if(ten_rad/coins[i].radius < tenratio[4]) vindex = 4;  //0.5 baht
+                else if(ten_rad/coins[i].radius < tenratio[5]) vindex = 5;  //0.25 baht
+
                 cout <<" | ";
                 ntype++;
                 if(diff_rad > min_diff) diff_rad *= weight_diff;
                 prev_rad = coins[i].radius;
             }
+
+            //find 10 Baht
+            if(!hasTen){
+                int b10 =  src.at<Vec3b>(Point(coins[i].centerx,coins[i].centery))[0];
+                int g10 =  src.at<Vec3b>(Point(coins[i].centerx,coins[i].centery))[1];
+                int r10 =  src.at<Vec3b>(Point(coins[i].centerx,coins[i].centery))[2];
+                int yellow_thres = 23;
+                if(r10 >= b10+yellow_thres && g10 >= b10+yellow_thres){
+                    hasTen = true;
+                    tentype = ntype;
+                    cout<<"(10)";
+                    ten_rad = prev_rad;
+                }
+            }
+            if(ntype == tentype){
+                vindex = 0;
+                cout<<"*";
+                //average 10Baht's radius
+                //ten_rad = (ten_rad+coins[i].radius)/2;
+            }
+
+            //Valid Coin
+            if(vindex>=0) valuencoin[value[vindex]]++;
+
             //print sortednumber
             cout<<coins[i].radius<<">";
         }
         cout<<endl<<"Number of Types : "<<ntype<<endl;
 
-        cout <<"Total Objects : "<<circles.size()<<endl;
+        cout <<"Total Coins : "<<circles.size()<<endl;
+        for (map<float,int>::iterator it = valuencoin.begin(); it != valuencoin.end(); ++it){
+            cout << it->first << " => " << it->second << '\n';
+            sum = sum + it->first*it->second;
+        }
+        cout <<"SUM : "<<sum<<endl;
         cout <<"============================================"<<endl;
         // Show your results
-        namedWindow( "Hough Circle "+to_string(p), CV_WINDOW_AUTOSIZE );
-        imshow( "Hough Circle "+to_string(p), src );
+        //namedWindow( "Hough Circle "+to_string(p), CV_WINDOW_AUTOSIZE );
+        //imshow( "Hough Circle "+to_string(p), src );
         //imshow( "Gray "+to_string(p), gray );
     }
     waitKey(0);
